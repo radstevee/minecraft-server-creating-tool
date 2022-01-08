e3616a0ac7b4ddab31ef2956e49532cfe72f1d19
Radsteve <96977790+radstevee@users.noreply.github.com>
2022-01-08 12:19:06 +0100

    added screen to the start.sh
    i forgot it lol

--- minecraft-server-creating-tool.sh
+++ minecraft-server-creating-tool.sh
@@ -2,6 +2,7 @@
 echo -e "IMPORTANT: This script requires openjdk-17-jdk and screen. (sudo apt install openjdk-17-jdk and sudo apt install screen)\nIf you don't have these packages, please cancel the script via CTRL+C."
 echo -e "\n"
 echo -e "\n"
+read -r -p "Please name your server: " NAME
 echo -e "Which server software should the server use? (Recommendation: Spigot)"
 echo "1) Vanilla"
 echo "2) Spigot"
@@ -38,7 +39,7 @@ echo "Creating your server...."
 
 curl -s https://minecraft-mirror.io/"$SOFTWARE"-"$VERSION".jar -o "$LOCATION"/server.jar
 
-echo 'java -jar server.jar nogui' > "$LOCATION"/start.sh
+echo 'screen -S $NAME java -jar server.jar nogui' > "$LOCATION"/start.sh
 echo 'eula=true' > "$LOCATION"/eula.txt
 
 chmod +x "$LOCATION"/start.sh
